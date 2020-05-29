5a8e6776f50441e0acae5406fe0af20b201b46fa
francisco <fluongo@gmail.com>
2020-05-29 09:47:49 -0700

    fixed an error in dataloader where train loaded 200 frames instead of 150

--- lightning_train.py
+++ lightning_train.py
@@ -66,7 +66,7 @@ class CustomDataset(Dataset):
 		if self.mode == 'train':
 			if n_frames > 300: # Sample random 200 frames
 				start_ii = random.choice(list(range(0, n_frames-300)))
-				video = video[start_ii:start_ii+399, :, :]
+				video = video[start_ii:start_ii+299, :, :]
 			start_phase = random.choice([0, 1])
 			video = video[list(range(start_phase, video.size()[0], 2)), :, :]
 		elif self.mode == 'val':
