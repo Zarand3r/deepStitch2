7f12a26195450e9c5bb6ff8c31d0f57349f2e989
Francisco Luongo <fluongo@gmail.com>
2020-06-04 17:54:51 -0700

    working version

--- preprocessing/generate_flows.sh
+++ preprocessing/generate_flows.sh
@@ -1,4 +1,4 @@
-for D in $(find /home/fluongo/code/usc_project/usc_data/balint/training_ready_updated/cfr_cut_mov -mindepth 2 -maxdepth 3) ; do
+for D in $(find /home/fluongo/code/usc_project/usc_data/balint/training_ready_updated/cfr_cut_mov_v2 -mindepth 2 -maxdepth 3) ; do
     echo $D 
     python convert_using_dali.py --mp4_fn $D --gpu_id 0
     python convert_using_flownet.py --mp4_fn $D --gpu_id 0
