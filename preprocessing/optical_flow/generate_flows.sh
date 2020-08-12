bf727d6a6ee0d26901813b20eae0f2216a7b045a
Richard Bao <richardbao419@gmail.com>
2020-08-11 23:06:39 -0700

    update

--- preprocessing/optical_flow/generate_flows.sh
+++ preprocessing/optical_flow/generate_flows.sh
@@ -1,6 +1,6 @@
 for D in $(find /central/groups/tensorlab/rbao/usc_data/classification_data -mindepth 2 -maxdepth 3) ; do
     echo $D 
-    python convert_using_dali.py --mp4_fn $D --gpu_id 0
+    # python convert_using_dali.py --mp4_fn $D --gpu_id 0
     python convert_using_flownet.py --mp4_fn $D --gpu_id 0
 done
 
