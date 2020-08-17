b3ed1534345f0910551d175515fab184800f56eb
Richard Bao <richardbao419@gmail.com>
2020-08-16 23:23:18 -0700

    update

---
+++ settings.py
@@ -0,0 +1,5 @@
+data_directory = '/central/groups/tensorlab/rbao/balint_data/cuts'
+data_labels = ''
+race_original = '/central/groups/tensorlab/rbao/balint_data/caltech_format'
+race_cut = '/central/groups/tensorlab/rbao/balint_data/caltech_format'
+
