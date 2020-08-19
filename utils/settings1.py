5fe03f9e9c732255a660fff6310f430a17a7b053
Richard Bao <richardbao419@gmail.com>
2020-08-18 21:26:50 -0700

    update

--- utils/settings1.py
+++ utils/settings1.py
@@ -1,5 +1,5 @@
 raw_directory = '/central/groups/tensorlab/rbao/robosurgery/balint_data/caltech_format'
 data_directory = '/central/groups/tensorlab/rbao/robosurgery/balint_data/classification_data'
-data_labels = '/central/groups/tensorlab/rbao/robosurgery/balint_data/RACE_python format final.xlsx' #file with timestamps
+data_labels = '/central/groups/tensorlab/rbao/robosurgery/balint_data/RACE_python_format_final.xlsx' #file with timestamps
 flownet_dir = '/central/groups/tensorlab/rbao/robosurgery/FlowNetPytorch'
 flownet_model = '/central/groups/tensorlab/rbao/robosurgery/FlowNetPytorch/trained_models/flownets_EPE1.951.pth.tar'
