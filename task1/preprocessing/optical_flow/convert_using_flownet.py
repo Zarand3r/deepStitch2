072a9423c25ea698f6b58198bf079f61d9daa58b
Richard Bao <richardbao419@gmail.com>
2020-08-28 14:05:40 -0700

    update

--- task1/preprocessing/optical_flow/convert_using_flownet.py
+++ task1/preprocessing/optical_flow/convert_using_flownet.py
@@ -45,7 +45,7 @@ parser = argparse.ArgumentParser(description='Conversion')
 parser.add_argument('--mp4_fn', default=settings1.raw_directory,type=str, help='input path')
 parser.add_argument('--gpu_id', default=1,type=int, help='which gpu')
 parser.add_argument('--window', default=4,type=int, help='sliding window for smoothing frames')
-parser.add_argument('--join', default=True,type=int, help='write optical flow side by side to original')
+parser.add_argument('--join', default=1,type=int, help='write optical flow side by side to original')
 args = parser.parse_args()
 
 model_dir = settings1.flownet_model
@@ -114,7 +114,7 @@ if __name__ == '__main__':
             rgb_flow = flow2rgb(div_flow * flow_output, max_value=max_flow)
             to_save = (rgb_flow * 255).astype(np.uint8).transpose(1,2,0)
 
-        if join:
+        if join > 0:
             combined = np.zeros([nY, nX*2, nC], dtype=np.uint8)
             combined[:, :nX, :] = mp4_ims[aa][:, :, [2,1,0]] # Invert back for BGR
             combined[:, nX:, :] = to_save
