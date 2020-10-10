917e8c064057eee7cdacadcd87fd1e3b555105cb
Richard Bao <richardbao419@gmail.com>
2020-10-09 19:50:43 -0700

    update

---
+++ task1/models/one_stream_rgb/predict.py
@@ -0,0 +1,21 @@
+# Load the preprocessed input video
+# If it has more frames than the sliding window, iteratively slice and classify each slice
+# Return a list of classifications per frame. The starting point of the slice that has a different label than before is the beginning of the new label.
+
+
+def load_ckp(checkpoint_fpath, model, optimizer):
+    """
+    checkpoint_path: path to save checkpoint
+    model: model that we want to load checkpoint parameters into       
+    optimizer: optimizer we defined in previous training
+    """
+    # load check point
+    checkpoint = torch.load(checkpoint_fpath)
+    # initialize state_dict from checkpoint to model
+    model.load_state_dict(checkpoint['state_dict'])
+    # initialize optimizer from checkpoint to optimizer
+    optimizer.load_state_dict(checkpoint['optimizer'])
+    # initialize valid_loss_min from checkpoint to valid_loss_min
+    valid_loss_min = checkpoint['valid_loss_min']
+    # return model, optimizer, epoch value, min validation loss 
+    return model, optimizer, checkpoint['epoch'], valid_loss_min.item()
