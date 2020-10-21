5a77edd76918e46f394c0ceb894bea49de8f2f2e
Richard Bao <richardbao419@gmail.com>
2020-10-20 19:55:12 -0700

    update

--- task1/models/two_stream/predict.py
+++ task1/models/two_stream/predict.py
@@ -1,3 +1,17 @@
+#%%
+import pandas as pd
+import os
+import argparse
+
+# Custom imports
+import git
+import sys
+repo = git.Repo("./", search_parent_directories=True)
+homedir = repo.working_dir
+sys.path.insert(1, f"{homedir}" + '/utils')
+import settings1
+
+
 # Load the preprocessed input video
 # If it has more frames than the sliding window, iteratively slice and classify each slice
 # Return a list of classifications per frame. The starting point of the slice that has a different label than before is the beginning of the new label.
@@ -19,3 +33,12 @@ def load_ckp(checkpoint_fpath, model, optimizer):
     valid_loss_min = checkpoint['valid_loss_min']
     # return model, optimizer, epoch value, min validation loss 
     return model, optimizer, checkpoint['epoch'], valid_loss_min.item()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    # Input argument
+    parser.add_argument("--data_labels", default = settings1.data_labels, help = "Path to labels")
+    parser.add_argument("--raw_directory", default = settings1.raw_directory, help = "Path to the raw data ")
+    parser.add_argument("--data_directory", default = settings1.data_directory, help = "Path to the output directory")
+    args = parser.parse_args()
+    splice(args)
