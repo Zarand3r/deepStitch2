f345ce38a2791548f04a7e6dc35fcbae8b7b619b
Richard Bao <richardbao419@gmail.com>
2020-11-11 00:50:41 -0700

    update

---
+++ task3/models/preprocessing/split.py
@@ -0,0 +1,3 @@
+# Make a function to split a segment classification by label
+# Split AB by label in column O
+# Reference the process_race spreadsheet. Each video has a unique name. There is one unique AB segment for each video. Put the name_AB videos with positive labels in positiveO, and those with negative in negativeO
