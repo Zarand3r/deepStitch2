0f97171d3a231813c984ca10d182dbbc83b9bf8e
Richard Bao <richardbao419@gmail.com>
2020-11-12 03:46:43 -0700

    update

---
+++ utils/settings3.py
@@ -0,0 +1,3 @@
+data_directory = '/home/richard_bao/robosurgery/balint_data/old_classification_data'
+output_directory = '/home/richard_bao/robosurgery/balint_data/label_classification_data'
+data_labels = '/home/richard_bao/robosurgery/balint_data/RACE_python_format_final.xlsx' #file with timestamps
