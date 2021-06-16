ad30f05c4869b76009c13def151651986f5685a9
Richard Bao <richardbao419@gmail.com>
2021-06-16 12:11:59 -0400

    update

--- processing/optical_flow/generate_flows.sh
+++
@@ -1,6 +0,0 @@
-for D in $(find /mnt/md1/richard_bao/balint_data/classification_data -mindepth 2 -maxdepth 3) ; do
-    echo $D 
-    # python convert_using_dali.py --mp4_fn $D --gpu_id 0
-    python convert_using_flownet.py --mp4_fn $D --gpu_id 0
-done
-
