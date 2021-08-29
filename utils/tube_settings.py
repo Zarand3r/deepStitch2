866448a3dc65fcaa2dc5645f966b7c1d3dc6e440
aloma85 <idrisun22@gmail.com>
2021-08-28 21:06:02 -0700

    setting for tube data

---
+++ utils/tube_settings.py
@@ -0,0 +1,34 @@
+#raw_directory = '/central/groups/tensorlab/rbao/robosurgery/balint_data/original'
+#data_directory = '/central/groups/tensorlab/rbao/robosurgery/balint_data/classification_data'
+#data_labels = '/central/groups/tensorlab/rbao/robosurgery/balint_data/RACE_python_format_final.xlsx' #file with timestamps
+#flownet_dir = '/central/groups/tensorlab/rbao/robosurgery/FlowNetPytorch'
+#flownet_model = '/central/groups/tensorlab/rbao/robosurgery/FlowNetPytorch/trained_models/flownets_EPE1.951.pth.tar'
+#checkpoints = '/central/groups/tensorlab/rbao/robosurgery/checkpoints/task1'
+
+#raw_directory = '/home/idris/robosurgery/balint_data/original'
+#data_directory = '/home/idris/robosurgery/balint_data/classification_data'
+#data_labels = '/home/idris/robosurgery/balint_data/RACE_python_format_final.xlsx' #file with timestamps
+#flownet_dir = '/home/idris/robosurgery/FlowNetPytorch'
+#flownet_model = '/home/idris/robosurgery/FlowNetPytorch/trained_models/flownets_EPE1.951.pth.tar'
+#checkpoints = '/home/idris/robosurgery/checkpoints/task1'
+
+# Rename label_TCN_data and label_directory to skills_TCN_data and skills_directory.
+
+original_directory = '/mnt/md1/richard_bao/tubes_data/original'
+raw_directory = '/mnt/md1/richard_bao/tubes_data/original/demonstrations'
+data_directory = '/mnt/md1/richard_bao/tubes_data/classification_data'
+label_directory = '/mnt/md1/richard_bao/tubes_data/label_classification_data'
+raw_kinematics_directory = '/mnt/md1/richard_bao/tubes_data/kinematics'
+kinematics_directory = '/mnt/md1/richard_bao/tubes_data/kinematics/demonstrations'
+raw_data_labels = '/mnt/md1/richard_bao/tubes_data/RACE_python_format_final.xlsx'
+data_labels = '/mnt/md1/richard_bao/tubes_data/RACE_python_format_final_demonstrations.csv' #file with timestamps
+flownet_dir = '/home/richard_bao/robosurgery/FlowNetPytorch'
+flownet_model = '/home/richard_bao/robosurgery/FlowNetPytorch/trained_models/flownets_EPE1.951.pth.tar'
+TCN_raw = '/mnt/md1/richard_bao/tubes_data/original/demonstrations'
+TCN_data = '/mnt/md1/richard_bao/tubes_data/TCN_data'
+label_TCN_data = '/mnt/md1/richard_bao/tubes_data/label_TCN_data'
+checkpoints1 = '/home/idris/robosurgery/checkpoints/tubes/task1'
+checkpoints2 = '/home/idris/robosurgery/checkpoints/tubes/task2'
+checkpoints3 = '/home/idris/robosurgery/checkpoints/tubes/task3'
+checkpoints4 = '/home/idris/robosurgery/checkpoints/tubes/task4'
+
